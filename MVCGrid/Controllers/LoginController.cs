df1ccd8729c4d8b26d0bba089dfb76e197aa108c
gopi-net <gopich2002@gmail.com>
2016-07-25 18:46:09 +0530

    Commit
    Login controller added

---
+++ MVCGrid/Controllers/LoginController.cs
@@ -0,0 +1,65 @@
+﻿using MVCGrid.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCGrid.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+        // Post: Login  using formcollection parameters
+        //[HttpPost]
+        //public ActionResult Login(FormCollection formObj)
+        //{
+        //    Response.Write("This is http request");
+        //    string uname = formObj["Uid"];
+        //    string pwd = formObj["Pwd"];
+        //    if (uname=="admin" && pwd=="admin")
+        //    {
+        //        Response.Write("<span>welcome to" + uname + "</span>");
+        //    }
+        //    else
+        //    {
+        //        Response.Write("<span>Invalid Username or Password");
+        //    }
+        //    return View();
+        //}
+
+        //Post: Login using formal parameters
+        //[HttpPost]
+        //public ActionResult Login(string Uid,string Pwd)
+        //{
+        //    if (Uid == "admin" && Pwd == "admin")
+        //    {
+        //        Response.Write("<span>welcome to" + Uid + "</span>");
+        //    }
+        //    else
+        //    {
+        //        Response.Write("<span>Invalid Username or Password");
+        //    }
+        //    return View();
+        //}
+
+        // Post: Login using model object parameters
+        [HttpPost]
+        public ActionResult Login(Login obj)
+        {
+            if (obj.Uid == "admin" && obj.Pwd == "admin")
+            {
+                Response.Write("<span>welcome to" + obj.Uid + "</span>");
+            }
+            else
+            {
+                Response.Write("<span>Invalid Username or Password");
+            }
+            return View();
+        }
+    }
+}
---
+++ MVCGrid/Models/Login.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCGrid.Models
+{
+    public  class Login
+    {
+        public string Uid { get; set; }
+        public string Pwd { get; set; }
+    }
+}

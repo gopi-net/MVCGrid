c28f12187759a47cec2cec885365e6c35d3b1f9c
Gopi.ch <gopikrishna.cherukumalli@raybiztech.com>
2016-08-02 19:17:14 +0530

    Commit

---
+++ MVCEF/Controllers/StudentController.cs
@@ -0,0 +1,23 @@
+﻿using MVCEF.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCEF.Controllers
+{
+    public class StudentController : Controller
+    {
+        StudentEntities db = new StudentEntities();
+
+        // GET: Student
+        public ActionResult Index()
+        {
+            StudentCourseVM obj = new StudentCourseVM();
+            obj.StudentList= db.Students.ToList();
+            obj.CourseList = db.Courses.ToList();
+            return View(obj);
+        }
+    }
+}
---
+++ MVCEF/Models/StudentCourseVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCEF.Models
+{
+    public class StudentCourseVM
+    {
+        public List<Student> StudentList { get; set; }
+        public List<Course> CourseList { get; set; }
+    }
+}
